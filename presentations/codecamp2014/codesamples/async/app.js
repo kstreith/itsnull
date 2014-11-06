304fde8e6cfa95606822d2aa5ed696efb225d7ca
Kip Streithorst <kstreith@yahoo.com>
2014-11-05 23:53:52 -0500

    More changes

--- presentations/codecamp2014/codesamples/async/app.js
+++ presentations/codecamp2014/codesamples/async/app.js
@@ -1,37 +1,18 @@
+var App = App || {};
+(function () {
+  App.Counter = 0;
   function sayHello() {
-    $("body").append("<div class='msg'>Hello, world</div");
-    setToClearLater();
+    App.Messages.Add("Hello", 1);
+    App.Messages.ClearLater(3, 3 * 1000);
   }
   function sayGoodbye() {
-    $("body").append("<div class='msg'>Goodbye</div");
-    setToClearLater();
+    App.Messages.Add("Goodbye", 2);
+    App.Messages.ClearLater(3, 3 * 1000); 
   }
-  function clearMessages() {
-      $(".msg").remove();
-  }
-  function setToClearLater() {
-    setTimeout(clearMessages, 3000);
+  function resetCount() {
+    App.Counter = 0;
   }
   $("#hello").on("click", sayHello); 
   $("#goodbye").on("click", sayGoodbye);
-  var obj = { UpdateCount: 2 };
-
-  function debugProp(obj, prop) {
-    var value = obj[prop];
-    var stopNext = true;
-    Object.defineProperty(obj, prop, {
-      get: function () {
-        return value;
-      },
-      set: function (val) {
-        value = val;
-        if (stopNext) {
-          stopNext = false;
-          debugger;
-        }
-      }
-    });
-  };
-  setInterval(function () {
-    obj.UpdateCount++;
-  }, 500);
+  $("#resetCount").on("click", resetCount);
+}());
